b795a6e86242b59f9f28b5d640162f7487213ced
Eshak Khan <92074702+eshakkhan29@users.noreply.github.com>
2022-08-30 19:31:25 +0600

    FETA cotact info

---
+++ src/components/Contact/ContactInfo.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FaMailBulk, FaPhoneAlt } from 'react-icons/fa';
+import { FiPhoneCall } from 'react-icons/fi';
+import { MdLocationOn } from 'react-icons/md';
+import { GiAmbulance } from 'react-icons/gi';
+
+const ContactInfo = () => {
+    return (
+        <div className='grid grid-cols-2 gap-x-[30px] gap-y-[80px]'>
+            <div className='shadow-[0px_0px_4px_2px_rgba(0,0,0,0.1)] bg-white p-[25px] flex flex-col items-center'>
+                <span className='w-10 h-10 text-2xl text-primary bg-white rounded-full border border-primary flex items-center justify-center'><FaPhoneAlt /></span>
+                <div className='mt-[19px] text-center'>
+                    <h4 className='text-xl font-bold'>Contact Number</h4>
+                    <p className='text-[#A1A1A1] text-base mt-[30px]'>+455 336 12312</p>
+                    <p className='text-[#A1A1A1] text-base'>+313 775 24663</p>
+                </div>
+            </div>
+            <div className='shadow-[0px_0px_4px_2px_rgba(0,0,0,0.1)] bg-white p-[25px] flex flex-col items-center'>
+                <span className='w-10 h-10 text-2xl text-primary bg-white rounded-full border border-primary flex items-center justify-center'><FaMailBulk /></span>
+                <div className='mt-[19px] text-center'>
+                    <h4 className='text-xl font-bold'>Email</h4>
+                    <p className='text-[#A1A1A1] text-base mt-[30px]'>healthhospital@gmail.com  </p>
+                </div>
+            </div>
+            <div className='shadow-[0px_0px_4px_2px_rgba(0,0,0,0.1)] bg-white p-[25px] flex flex-col items-center'>
+                <span className='w-10 h-10 text-2xl text-primary bg-white rounded-full border border-primary flex items-center justify-center'><MdLocationOn /></span>
+                <div className='mt-[19px] text-center'>
+                    <h4 className='text-xl font-bold'>Location</h4>
+                    <p className='text-[#A1A1A1] text-base mt-[30px]'>Sesame Street <br /> London, United Kingdom</p>
+                </div>
+            </div>
+            <div className='shadow-[0px_0px_4px_2px_rgba(0,0,0,0.1)] bg-white p-[25px] flex flex-col items-center'>
+                <div className='flex items-center gap-x-[11px]'>
+                    <span className='w-10 h-10 text-2xl text-primary bg-white rounded-full border border-primary flex items-center justify-center'><FiPhoneCall /></span>
+                    <span className='text-[36px] text-primary'>+</span>
+                    <span className='w-10 h-10 text-2xl text-primary bg-white rounded-full border border-primary flex items-center justify-center'><GiAmbulance /></span>
+                </div>
+                <div className='mt-[19px] text-center'>
+                    <h4 className='text-xl font-bold'>Emergency/Ambulance Service</h4>
+                    <p className='text-[#A1A1A1] text-base mt-[30px]'>+265 196 79537</p>
+                    <p className='text-[#A1A1A1] text-base'>+123 838 35777</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ContactInfo;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import ContactInfo from '../components/Contact/ContactInfo';
+import Appointment from '../components/utility/Appointment';
 import Breadcrumbs from '../components/utility/Breadcrumbs';
 import Footer from '../components/utility/Footer';
 import Header from '../components/utility/Header';
 import PageBanner from '../components/utility/PageBanner';
+import PageLayout from '../pageLayout/PageLayout';
 
 const Contact = () => {
     const location = useLocation();
@@ -13,7 +16,8 @@ const Contact = () => {
             <PageBanner>
                 Contact
             </PageBanner>
-            <Breadcrumbs location={location}/>
+            <Breadcrumbs location={location} />
+            <PageLayout left1={<Appointment />} right={<ContactInfo/>} />
             <Footer />
         </div>
     );
